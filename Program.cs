5155a6385d76bde3d171269f6d7a8a05bd80e00d
Ritisha <Ritisha@DESKTOP-5NA9075>
2020-02-16 20:20:25 -0500

    added one more line

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace TestApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!test");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This can be modified as requeired");
         }
     }
 }
